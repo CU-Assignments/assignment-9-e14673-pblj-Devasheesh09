2d7b3c4fb2f61568ba84d9c5e2eaa24c73c8735b
Devasheesh09 <165001141+Devasheesh09@users.noreply.github.com>
2025-04-18 11:04:08 +0530

    Create hardmain.java

---
+++ hardmain.java
@@ -0,0 +1,10 @@
+package com.bank;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.bank.service.BankingService;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context
